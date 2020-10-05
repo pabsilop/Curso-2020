a5d741c4f2a32f43701b05f699a19347aedc2b4e
Pablo Silva López <pablosilva2705@hotmail.com>
2020-10-05 17:06:47 +0200

    .

---
+++ Acceso a Datos/Thymeleaf/src/main/java/com/thymeleaf/Thymeleaf/MainController.java
@@ -0,0 +1,5 @@
+package com.thymeleaf.Thymeleaf;
+
+public class MainController {
+
+}
---
+++ Acceso a Datos/Thymeleaf/src/test/java/com/thymeleaf/Thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleaf.Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
