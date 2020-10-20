260f4a814cf11745b11d42df96f35d462adce26c
Pablo Silva López <pablosilva2705@hotmail.com>
2020-10-21 01:17:58 +0200

    .

--- Sistemas de Gestion/BoletinEjercicios2/Ejercicio7.py
+++ Sistemas de Gestion/BoletinEjercicios2/Ejercicio7.py
@@ -7,102 +7,88 @@
 #Con valores introducidos por el usuario, que deben formar una secuencia creciente.
 #Con valores introducidos por el usuario, que no deben estar repetidos.
 
-#Las ejecuciones de las funciones están comentadas
 import random
+listaNumeros = []
 
-lista = []
-
-#Con valores aleatorios entre 1 y 10, y a continuación diga cuantos pares e impares hay.
-
-def aleatorio_impares():
-    impares = 0
-    for x in range(15):
-        num = random.randrange(10)
-        lista.append(num)
-        if (num % 2 != 0):
-            impares += 1
-    print(lista)
-    print("Hay ", impares, " numeros impares")
-
-#aleatorio_impares()
-
-#Con valores aleatorios entre 1 y 10, y a continuación sume los que estén en posiciones que son múltiplos de 3.
-def aleatorio_suma():
-    res = 0
-    for x in range(15):
-        num = random.randrange(10)
-        lista.append(num)
-    for x in range(len(lista)):
-        if (x % 3 == 0):
-            res += lista[x]
-    print(lista)
-    print("La suma de los números en una posición múltiplo de 3 es ", res)
-
-#aleatorio_suma()
-
-#Con los primeros valores de la serie de Fibonacci.
-
-
-
-def fibonacci():
-    n1=0
-    n2=1
+def numerosAletoriosParEImpar(listaNumeros):
+    nPares=0
+    nImpares = 0
     for i in range(15):
-        n3=n1+n2
-        lista.append(n3)
-        n1=n2
-        n2=n3
-    print(lista)
-#fibonacci()
+        num = random.randrange(10)
+        listaNumeros.append(num)
+        if (num % 2 == 0):
+            nPares += 1
+        if (num % 2 == 1):
+            nImpares += 1
+    print(listaNumeros)
+    print("Pares: ",nPares)
+    print("Impares: ",nImpares)
 
-#Con valores introducidos por el usuario, y a continuación que los imprima al revés.
-def revertir():
+
+
+def randomConSuma():
+    restoDivision = 0
+    for i in range(15):
+        num = random.randrange(10)
+        listaNumeros.append(num)
+    for i in range(len(listaNumeros)):
+        if (i % 3 == 0):
+            restoDivision += listaNumeros[i]
+    print(listaNumeros)
+    print(f"Los números que se encuentran en posicones que son multiplos de 3 suman: {restoDivision}")
+
+
+
+def conFibonacci():
+    a=0
+    b=1
+    for i in range(15):
+        c=a+b
+        listaNumeros.append(c)
+        a=b
+        b=c
+    print(listaNumeros)
+
+def delReves():
     for x in range(15):
-        num = int(input("Introduzca un número: "))
-        lista.append(num)
-    for i in reversed(lista):
-        print(i, end=" ")
-#revertir()
+        introducir = int(input("Que número quiere introducir: "))
+        listaNumeros.append(introducir)
+    for i in reversed(listaNumeros):
+        print(i)
 
 #Con valores introducidos por el usuario, donde cada valor se debe pedir de nuevo hasta que esté entre 1 o 10.
 
-def menor_diez():
+def introducirMayor10():
     num = 0
-    for x in range(15):
-        num = int(input("Introduzca un número: "))
-        while num < 1 or num > 10:
-            print("Número no válido")
-            num = int(input("Introduzca un número: "))
-        lista.append(num)
-    print(lista)
-#menor_diez()
+    for i in range(15):
+        introducir = int(input("Diga Número: "))
+        while introducir < 1 or introducir > 10:
+            print("El número introducido tiene que estar entre el número 1 y el 10, intentelo de nuevo")
+            introducir = int(input("Diga Número: "))
+        listaNumeros.append(introducir)
+    print(listaNumeros)
 
 #Con valores introducidos por el usuario, que deben formar una secuencia creciente.
 
-def seq_creciente():
-    num1 = 0
-    num2 = 0
-    for x in range(15):
-        num2 = num1
-        num1 = int(input("Introduzca un número: "))
-        while num1 < num2:
-            print("Número no válido")
-            num1 = int(input("Introduzca un número: "))
-        lista.append(num1)
-    print(lista)
+def crecientes():
+    a, b = 0, 0
+    for i in range(15):
+        b = a
+        a = int(input("Diga Número: "))
+        while a <= b:
+            print("El número tiene que ser mayor que el anterior, vuelva a intentarlo")
+            a = int(input("Diga Número: "))
+        listaNumeros.append(a)
+    print(listaNumeros)
 
-#seq_creciente()
+def repetidos():
+    introducido = 0
+    for i in range(15):
+        introducido = int(input("Diga número: "))
+        while introducido in listaNumeros:
+            print("Este número ya se encuentra en la lista, tienes que poner uno que no esté, vuelve a intentarlo")
+            introducido = int(input("Diga número: "))
+        listaNumeros.append(introducido)
+    print(listaNumeros)
 
-#Con valores introducidos por el usuario, que no deben estar repetidos.
 
-def lista_no_repetidos():
-    num = 0
-    for x in range(15):
-        num = int(input("Introduzca un número: "))
-        while num in lista:
-            print("Número no válido")
-            num = int(input("Introduzca un número: "))
-        lista.append(num)
-    print(lista)
-
-#lista_no_repetidos()
